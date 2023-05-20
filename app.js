df9fedd0dd56814b2c9379bd34c3265ddd3d6c12
raghad2410 <raghadnarmoq@gmail.com>
2023-05-20 21:09:14 +0000

    Assignment 08

--- app.js
+++ app.js
@@ -7,27 +7,31 @@ function EmployeeInformation(EmployeeID, fullName, department, level) {
     this.level = level;
 
     this.salary = this.calcSalary();
-    this.netSalary = 0.925 * this.salary
+    this.netSalary = Math.round(0.925 * this.salary)
 
-    this.para = `The Full Name is ${this.fullName}, the employee ID is ${this.EmployeeID}, the employee department is ${this.department},
-    the level is ${this.level} and the salary is ${this.salary} and the salary with tax 7.5% will be ${this.netSalary}`;
-
+    this.para = `Employee name: ${this.fullName} `;
+    this.sal = `Employee salary : ${this.salary}, Employee salary with 7.5% tax : ${this.netSalary}`
     arr.push(this);
+    console.log(this) //object
 }
+console.log(arr) //array
 
 EmployeeInformation.prototype.calcSalary = function () {
     if (this.level === "Senior") {
         min = 1500;
         max = 2000;
-        return (Math.floor(Math.random() * (max - min + 1)) + min);
+        finalSalary = Math.floor(Math.random() * (max - min + 1)) + min
+        return (Math.round(finalSalary));
     } else if (this.level === "Mid-Senior") {
         min = 1000;
         max = 1500;
-        return (Math.floor(Math.random() * (max - min + 1)) + min);
+        finalSalary = Math.floor(Math.random() * (max - min + 1)) + min
+        return (Math.round(finalSalary));
     } else if (this.level === "Junior") {
         min = 500;
         max = 1000;
-        return (Math.floor(Math.random() * (max - min + 1)) + min);
+        finalSalary = Math.floor(Math.random() * (max - min + 1)) + min
+        return (Math.round(finalSalary));
     }
 }
 
@@ -39,9 +43,18 @@ const emp1004 = new EmployeeInformation(1004, "Omar Zaid", "Development", "Senio
 const emp1005 = new EmployeeInformation(1005, "Rana Saleh", "Development", "Junior");
 const emp1006 = new EmployeeInformation(1006, "Hadi Ahmad", "Finance", "Mid-Senior");
 
+
+
 for (let i = 0; i < arr.length; i++) {
     const newP = document.createElement("p");
-    newP.textContent = arr[i].para;
+    newP.textContent = `${arr[i].para}`;
+    const newPP = document.createElement("p");
+    newPP.textContent = `${arr[i].sal}`;
+
+    const parentPpp = document.createElement("div");
+    parentPpp.appendChild(newP)
+    parentPpp.appendChild(newPP)
+
     const parentElement = document.getElementById("list");
-    parentElement.appendChild(newP);
+    parentElement.appendChild(parentPpp);
 }
